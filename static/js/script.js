eaf53215ec5ceb3b9d9bed360989275e3dcd52b5
FruitbatM <mar.pavlovic@gmail.com>
2021-06-07 20:07:35 +0000

    Remove carousel and add slider functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,12 @@
 // jQuery for MaterializeCSS initialization
 
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.carousel').carousel(
-      {
-        dist: 0,
-        padding: 0,
-        fullWidth: true,
-        indicators: true,
-        duration: 200,
-      }
-      );
-  });
-
-  // auto-play functionality with delay of 5 seconds
-  function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 5000);
-   }    
-   setTimeout(autoplay, 5000);
+  $('.sidenav').sidenav({edge: "right"});
+  $('.slider').slider(
+    {
+      indicators: true,
+      interval: 6000,
+      duration: 800,
+    }
+  );
+});
