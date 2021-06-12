080a4e1ff77116704e67bef95c76ae76c71001e4
FruitbatM <mar.pavlovic@gmail.com>
2021-06-12 20:40:43 +0000

    Add add_doctor template

--- app.py
+++ app.py
@@ -114,18 +114,18 @@ def logout():
 def doctor_login():
     if request.method == "POST":
         # check if username exists in the database
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+        existing_doctor = mongo.db.doctors.find_one(
+            {"email": request.form.get("email").lower()})
 
-        if existing_user:
+        if existing_doctor:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                existing_doctor["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(
-                        request.form.get("username")))
+                        request.form.get("email")))
                     return redirect(url_for(
-                        "profile", username=session["user"]))
+                        "doctor_profile", email=session["user"]))
 
             else:
                 # invalid password match
@@ -143,6 +143,11 @@ def doctor_login():
 def get_doctors():
     doctors = mongo.db.doctors.find()
     return render_template("doctor_profile.html", doctors=doctors)
+
+
+@app.route("/add_doctor", methods=["GET", "POST"])
+def add_doctor():
+    return render_template("add_doctor.html")
 
 
 if __name__ == "__main__":
