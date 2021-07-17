df4392012e1bb445f4a1853dcc8218183e871745
FruitbatM <mar.pavlovic@gmail.com>
2021-07-17 16:32:45 +0000

    Add back to top arrow functionality

--- static/js/script.js
+++ static/js/script.js
@@ -60,3 +60,29 @@ $(document).ready(function(){
     }
 });
 
+
+// Back to Top Arrow - code with modification taken from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+mybutton = document.getElementById('arrow_2top');
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50|| document.documentElement.scrollTop > 50) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Tooltip
+// Code source: https://getbootstrap.com/docs/4.6/components/tooltips/-->
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
